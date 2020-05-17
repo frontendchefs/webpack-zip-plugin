46ef29987c40cbda046c234f9d86b56afe3eeecd
udaypydi <udaypydi333@gmail.com>
2020-05-17 23:46:09 +0530

    add compilation hook

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
 class WebpackZipPlugin {
     apply(compiler) {
         compiler.hooks.afterEmit.tap('WebpackZipPlugin', (stats) => {
-            
+            const filePath = stats.compilation.options.output.path;
         });
     }
 }
